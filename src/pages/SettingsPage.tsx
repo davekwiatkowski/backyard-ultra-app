c7ccec275b9dc31f12b95d5bf81a5e5f420411a6
Dave Kwiatkowski <github@davekwiatkowski.com>
2023-08-08 16:59:14 -0400

    Add race start time to settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,14 +1,24 @@
-import { Layout, Typography } from 'antd';
+import { Divider, Layout, Space, Typography } from 'antd';
 import EndRaceButton from '../components/EndRaceButton';
 import { Content, Footer } from 'antd/es/layout/layout';
 import dayjs from 'dayjs';
+import { useContext } from 'react';
+import RaceContext from '../store/RaceContext';
 
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
 const SettingsPage = () => {
+  const { startTime } = useContext(RaceContext);
+
   return (
     <Layout style={{ height: '100%' }}>
       <Content>
+        <Title level={2}>Info</Title>
+        <Title level={5}>Race start time</Title>
+        <Text type='secondary'>
+          {new Date(startTime.valueOf()).toLocaleString()}
+        </Text>
+        <Divider />
         <EndRaceButton />
       </Content>
       <Footer>
