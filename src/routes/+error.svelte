200760fe26e130e71af89d9bbb31f3de4519cf96
Dave <github@davekwiatkowski.com>
2024-06-08 10:41:56 -0400

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+	import { page } from '$app/stores';
+</script>
+
+<style>
+	.status {
+		color: red
+	}
+</style>
+
+<h1 class='title'>Uh oh! There was an error...</h1>
+<h2 class='status'>{$page.status}</h2>
+<p>{$page.error?.message}</p>
