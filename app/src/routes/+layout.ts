075881fe276caf36d5da7dd16a4b95635e8bc581
Dave <github@davekwiatkowski.com>
2024-06-08 13:17:22 -0400

    do not publish backyard rankings

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -1,1 +1,8 @@
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ fetch }) {
+    const res = await fetch('/data/backyard-ultra-rankings.json');
+    const backyardUltraRankings = await res.json();
+    return { backyardUltraRankings };
+}
+
 export const prerender = true;
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+</script>
+
 <h1>backyardultra.app</h1>
 <p>The backyard ultra app</p>
+
+<ul>
+    {#each data.backyardUltraRankings as item}
+        <li>
+            <span>{item.date}</span>
+            |
+            <span>{item.race}</span>
+            |
+            <span>{item.name}</span>
+            |
+            <span>{item.yards} yards</span>
+            |
+            <span>({item.window})</span>
+        </li>
+    {/each}
+</ul>
