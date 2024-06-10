273c1d6fe9eb767312282ee431db9f641c2970d2
Dave <github@davekwiatkowski.com>
2024-06-10 00:45:27 -0400

    fix: data size issue?

--- app/src/routes/+layout.ts
+++ app/src/routes/+layout.ts
@@ -2,8 +2,9 @@ import type { IRanking } from '../types/IRanking.js';
 import type { LayoutLoadEvent } from './$types.js';
 
 export async function load({ fetch }: LayoutLoadEvent) {
+    const personRankingsData: { [name: string]: IRanking[] } = await (await fetch('/data/person-rankings.json')).json();
     const dateWindowRankingsData: { [window: string]: IRanking[] } = await (await fetch('/data/date-window-rankings.json')).json();
-    return { dateWindowRankingsData };
+    return { personRankingsData, dateWindowRankingsData };
 }
 
 export const prerender = true;
--- app/src/routes/person/[slug]/+layout.ts
+++ app/src/routes/person/[slug]/+layout.ts
@@ -1,7 +1,6 @@
 import type { IRanking } from '../../../types/IRanking';
 import type { LayoutLoad } from './$types';
 
-export const load = (async ({ fetch, params }) => {
-    const personRankingsData: { [name: string]: IRanking[] } = await (await fetch('/data/person-rankings.json')).json();
-    return { personRankingsData, currentPersonId: params.slug };
+export const load = (async ({ params }) => {
+    return { currentPersonId: params.slug };
 }) satisfies LayoutLoad;
