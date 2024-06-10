9eaf151b4157bc7759bffe106e7c71e8718ffdab
Dave <github@davekwiatkowski.com>
2024-06-10 16:12:01 -0400

    feat: add table

--- app/src/routes/window/[slug]/+page.svelte
+++ app/src/routes/window/[slug]/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
-    import { base } from "$app/paths";
     import type { PageData } from "./$types";
+    import DataTable from "./data-table.svelte";
 
     export let data: PageData;
 </script>
 
-<ul>
-    {#each data.windowRankingsData as item}
-        <li>
-            <span>Rank {item.rank}</span>
-            {#if item.isRankTied}
-                <span>(tie)</span>
-            {/if}
-            |
-            <a href={`${base}/person/${item.id}`}>
-                <span>{item.name}</span>
-                {#if item.nationality}
-                    <span>({item.nationality})</span>
-                {/if}
-            </a>
-            |
-            <span>{item.race}</span>
-            |
-            <span>{item.yards} yards</span>
-            (<span>{item.date}</span>)
-        </li>
-    {/each}
-</ul>
+<div class="container mx-auto py-10">
+    <DataTable data={data.windowRankingsData} />
+</div>
