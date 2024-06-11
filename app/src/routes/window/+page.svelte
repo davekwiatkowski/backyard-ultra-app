6973e9c123234b6be1181956569d8dc42ff71230
Dave <github@davekwiatkowski.com>
2024-06-11 00:16:04 -0400

    fix: change button to link

--- app/src/routes/window/+page.svelte
+++ app/src/routes/window/+page.svelte
@@ -1,26 +1,12 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
     import { base } from "$app/paths";
-    import { Button } from "$lib/components/ui/button/index.js";
 
     export let data;
 </script>
 
 <strong>Date window:</strong>
-<div class="keys">
+<div class="flex flex-wrap flex-row">
     {#each data.windowKeys as key}
-        <Button variant="link" on:click={() => goto(`${base}/window/${key}`)}>
-            {key}
-        </Button>
+        <a class="p-2" href={`${base}/window/${key}`}>{key}</a>
     {/each}
 </div>
-
-<slot />
-
-<style>
-    .keys {
-        display: flex;
-        flex-wrap: wrap;
-        flex: row;
-    }
-</style>
